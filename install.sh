0408670aaf3c5429e2c6f08c931f09db92500494
Jose <sanjuanjojo@gmail.com>
2015-08-23 18:10:02 +0800

    Change install.sh from nicknisi/dotfiles

--- install.sh
+++ install.sh
@@ -5,37 +5,7 @@ echo "Installing dotfiles..."
 #echo "Initializing submodule(s)"
 #git submodule update --init --recursive
 
-DOTFOLDER="$HOME/.dotfiles"
-ZSHRC=".zshrc"
-GITCONF=".gitconfig"
-DIR=""
+source install/link.sh
 
-if [ ! -d "$DOTFOLDER" ]; then
-    mkdir "$DOTFOLDER"
-fi
 
-echo "Creating symlinks"
-linkables=$( ls -1 -d **/*.symlink )
-
-for file in $linkables ; do
-    name="$( basename $file ".symlink" )"
-    target="$HOME/.$name"
-    echo "Creating symlink for $file"
-
-    if [-d "$file" ] ; then
-        DIR="-r"
-    fi 
-
-    cp $DIR $file $DOTFOLDER/$name
-    ln -s $DOTFOLDER/$name $target
-
-    DIR=""
-done
-
-cp $ZSHRC $DOTFOLDER
-ln -s $DOTFOLDER/$ZSHRC $HOME/$ZSHRC
-
-cp $GITCONF $DOTFOLDER
-ln -s $DOTFOLDER/$GITCONF $HOME/$GITCONF
-
-echo "All dotfiles installed"
+echo "Installation Complete!"
