a36095e79fab8b6a97d7274d932243a0672c0343
Jose San Juan <sanjuanjojo@gmail.com>
2015-08-24 23:54:18 +0800

    Add color on ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,14 @@ alias reload!='source ~/.zshrc'
 # Detect which `ls` flavor is in use
 if ls --color > /dev/null 2>&1; then # GNU `ls`
     colorflag="--color"
+
+else # OS X `ls`
+    colorflag="-G"
+fi
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+    colorflag="--color"
 else # OS X `ls`
     colorflag="-G"
 fi
@@ -14,7 +22,10 @@ alias ...='cd ../..'
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
-alias ll='ls -la'
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
 
 # tmux
 alias tmux='tmux -2'
