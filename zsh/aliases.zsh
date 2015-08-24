93a1dc6259b0b924bf2bd859951815d6e9ad6a77
Jose San Juan <sanjuanjojo@gmail.com>
2015-08-24 23:27:45 +0800

    Zsh config from nicknisi/dotfiles/zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,25 @@
+# reload zsh config
+alias reload!='source ~/.zshrc'
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+    colorflag="--color"
+else # OS X `ls`
+    colorflag="-G"
+fi
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias ll='ls -la'
+
+# tmux
+alias tmux='tmux -2'
+alias ta='tmux attach'
+alias tls='tmux ls'
+alias tat='tmux attach -t'
+alias tns='tmux new-session -s'
+
---
+++ zsh/config.zsh
@@ -0,0 +1,24 @@
+setopt NO_BG_NICE
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS
+setopt LOCAL_TRAPS
+#setopt IGNORE_EOF
+setopt PROMPT_SUBST
+
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt hist_ignore_space
+
+# history
+setopt HIST_VERIFY
+setopt EXTENDED_HISTORY
+setopt HIST_REDUCE_BLANKS
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt INC_APPEND_HISTORY SHARE_HISTORY
+setopt APPEND_HISTORY
+
+setopt COMPLETE_ALIASES
+
