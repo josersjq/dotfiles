3c89881f0eaa658e9c46936f0e14dedff1528e2f
Jose San Juan <sanjuanjojo@gmail.com>
2015-08-25 20:52:06 +0800

    Fix grep error

--- sys/load_ssh_identities.sh
+++ sys/load_ssh_identities.sh
@@ -8,7 +8,7 @@ SSH_KEYS=(
     );
 
 function start_agent() {
-#    echo "Initialising new SSH agent..."
+    echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
 #    echo "succeeded"
 
@@ -22,7 +22,7 @@ function start_agent() {
 }
 
 if [ -f "${SSH_ENV}" ]; then
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+    ps -ef | grep '${SSH_AGENT_PID}' | grep ssh-agent$ > /dev/null || {
         start_agent;
     }
 else
