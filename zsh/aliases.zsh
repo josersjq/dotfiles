edc02fd1c8dad9a1a5106df1537921728a58f360
Jose San Juan <sanjuanjojo@gmail.com>
2015-08-26 07:29:21 +0800

    Add nvm&virtualenv aliases, and apt alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,6 +18,8 @@ alias l="ls -lFh ${colorflag}"
 alias la="ls -AF ${colorflag}"
 alias ll="ls -lah ${colorflag}"
 
+alias apt-get="sudo apt-get"
+
 # tmux aliases
 alias tmux='tmux -2'
 alias ta='tmux attach'
@@ -27,3 +29,10 @@ alias tns='tmux new-session -s'
 
 # virtualenv
 alias da='deactivate'
+# if existing virtualenv!
+alias scr='workon scrapy'
+alias dja='workon django'
+# nvm
+alias nu='nvm use'
+alias nd='nvm deactivate'
+alias n1='nvm use 0.12.7'
