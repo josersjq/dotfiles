697b486febeb4b4772f221c327cca3cd2e17db8b
Jose San Juan <sanjuanjojo@gmail.com>
2015-08-28 21:43:47 +0800

    remove virtualenv install script

--- install/virtualenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -d $HOME/.virtualenvs ]; then
-    echo "Configure virtualenv prompt"
-    cat "$HOME/.dotfiles/virtualenvs/postactivate" > "$HOME/.virtualenvs/postactivate"
-    cat "$HOME/.dotfiles/virtualenvs/postdeactivate" > "$HOME/.virtualenvs/postdeactivate"
-fi
