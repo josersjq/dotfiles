83694576c3b423468410c5f1b2f79a6302ebe117
Jose <sanjuanjojo@gmail.com>
2015-11-24 17:46:20 +0800

    Silence echo and identity added

--- sys/load_ssh_identities.sh
+++ sys/load_ssh_identities.sh
@@ -8,22 +8,20 @@ SSH_KEYS=(
     );
 
 function start_agent() {
-    echo "Initialising new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-#    echo "succeeded"
-
+    /usr/bin/ssh-agent | sed  's/^echo/#echo/' > "${SSH_ENV}"
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
 
     for i in ${SSH_KEYS[@]}
     do
-        /usr/bin/ssh-add $i
+        /usr/bin/ssh-add $i>/dev/null 2>&1
     done
 }
 
 if [ -f "${SSH_ENV}" ]; then
     ps -ef | grep '${SSH_AGENT_PID}' | grep ssh-agent$ > /dev/null || {
         start_agent;
+
     }
 else
     start_agent;
