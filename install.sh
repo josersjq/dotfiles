31913e9647aec03ba41a04e2345fb7400ae7fc93
Jose <sanjuanjojo@gmail.com>
2018-01-24 14:56:14 +0800

    Add apt-vim configuration

--- install.sh
+++ install.sh
@@ -5,20 +5,21 @@ echo " Choose from"
 
 select environment in virtualmachine desktop exit
 do
-	case $environment in
-	virtualmachine|desktop|exit)
-		break
-		;;
-	*)
-		echo "Invalid environment"
-		;;
-	esac
+    case $environment in
+    virtualmachine|desktop|exit)
+            break
+            ;;
+    *)
+            echo "Invalid environment"
+            ;;
+    esac
 done
 
 if [ $environment = "exit" ]; then
-	exit 0
+    exit 0
 else
-	source install/link.sh $environment
+    source install/link.sh $environment
+    source install/apt-vim.sh
 fi
 
 echo "Successfuly installed $environment dotfiles"
---
+++ install/apt-vim.sh
@@ -0,0 +1,2 @@
+curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
+source ~/.bashrc
