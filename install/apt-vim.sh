4598c229f3d7cb1a556ca9f6fd09e8a32d2c40f4
Jose <sanjuanjojo@gmail.com>
2018-01-24 15:23:33 +0800

    Remove .vimpkg, Update apt-vim.sh

--- install/apt-vim.sh
+++ install/apt-vim.sh
@@ -1,2 +1,5 @@
 curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
 source ~/.bashrc
+rm ~/.vimpkg/vim_config.json
+ln -s ~/.dotfiles/virtualmachine/vim/vim_config.json ~/.vimpkg/vim_config.json
+apt-vim install
