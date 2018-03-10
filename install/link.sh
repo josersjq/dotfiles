1a4c6fbc7310d5f7222e79a4b62d13dd52ef143e
Jose <sanjuanjojo@gmail.com>
2018-03-10 20:15:37 +0800

    Update checking of target file or dir

--- install/link.sh
+++ install/link.sh
@@ -4,13 +4,13 @@ DOTFILES=$1/$2
 
 echo "Creating symlinks"
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
-for file_or_dir in $linkables ; do
-    target="$HOME/.$( basename $file_or_dir ".symlink" )"
-    echo "Creating symlink for $file_or_dir"
-    if [[ -d $file_or_dir ]]; then
-        rm -rf $file_or_dir;
-    elif [[ -f $file_or_dir ]]; then
-        rm $file_or_dir;
+for file in $linkables ; do
+    target="$HOME/.$( basename $file ".symlink" )"
+    echo "Creating symlink for $file"
+    if [[ -d $target ]]; then
+        rm -rf $target;
+    elif [[ -f $target ]]; then
+        rm $target;
     fi
-    ln -s $file_or_dir $target
+    ln -s $file $target
 done
