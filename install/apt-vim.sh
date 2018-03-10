1f7dc19348a072c3ba34a4a04840df013b5fb58b
Jose <sanjuanjojo@gmail.com>
2018-03-10 20:17:47 +0800

    Updated apt-vim setup

--- install/apt-vim.sh
+++ install/apt-vim.sh
@@ -1,6 +1,6 @@
 curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
-source ~/.bashrc
 rm ~/.vimpkg/vim_config.json
 ln -s $1/$2/vim/vim_config.json ~/.vimpkg/vim_config.json
+source ~/.bashrc
 apt-vim install -y
 git checkout .
