e944f1260215d8275bb56e82ca316ad1d6d9b667
Jose <sanjuanjojo@gmail.com>
2018-03-10 20:13:30 +0800

    Add checking of file or directory

--- install/link.sh
+++ install/link.sh
@@ -4,8 +4,13 @@ DOTFILES=$1/$2
 
 echo "Creating symlinks"
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
-for file in $linkables ; do
-    target="$HOME/.$( basename $file ".symlink" )"
-    echo "Creating symlink for $file"
-    ln -s $file $target
+for file_or_dir in $linkables ; do
+    target="$HOME/.$( basename $file_or_dir ".symlink" )"
+    echo "Creating symlink for $file_or_dir"
+    if [[ -d $file_or_dir ]]; then
+        rm -rf $file_or_dir;
+    elif [[ -f $file_or_dir ]]; then
+        rm $file_or_dir;
+    fi
+    ln -s $file_or_dir $target
 done
