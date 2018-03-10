85c6e5486b7f2d6691e7595e5faff6b70a851e04
Jose <sanjuanjojo@gmail.com>
2018-03-10 20:42:18 +0800

    Remove NVM and Node Dependency on apt-vim installation, add sh file for NVM and Node Instructions

---
+++ install/nvm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install NVM using this command:
+curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
+# then install Node using:
+nvm install node
+nvm install iojs
