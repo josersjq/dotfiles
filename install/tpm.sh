73bc1d9fa65dbe89a017dccb09ca9a077a7df52b
Jose San Juan <jojo@icannhas.com>
2019-01-30 21:52:05 +0800

    Add output, ref, and instructions for tpm install file

--- install/tpm.sh
+++ install/tpm.sh
@@ -1,7 +1,11 @@
+# ref
+# https://github.com/tmux-plugins/tpm#installation
 target="$HOME/.tmux/plugins/tpm"
 
 if [[ ! -d $target ]]; then
     git clone https://github.com/tmux-plugins/tpm $target
 fi
 
-
+echo "Now open tmux and do this cmd `tmux source ~/.tmux.conf`"
+echo "After that and you're still inside tmux press <prefix> I"
+echo "done!"
